3eb9d01abaa0fad0f4886b08c484c073dff36ae1
Mariano <marianobarr@heep-nite.com>
2021-03-28 23:41:00 -0400

    adding review shortcode

--- assets/go-script.js
+++ assets/go-script.js
@@ -1,7 +1,44 @@
 (function($) {
+
+    /* Slider Reviews */
+    $('#list-review-ul').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        prevArrow: '<div id="prev-slide-arrow-rev" class="prev-slide-arrow-rev">&larr;</div>',
+        nextArrow: '<div id="next-slide-arrow-rev" class="next-slide-arrow-rev">&rarr;</div>',
+        responsive: [{
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+
+                }
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    var childReview = $('.slick-track > .slick-active');
+    $('.slick-track > .slick-active').removeClass('thisItem');
+    childReview.eq(2).addClass('thisItem')
+        /*  */
+    $('#next-slide-arrow-rev').on('click', function() {
+        console.log('si es igjual a 3');
+        var childReview = $('.slick-track > .slick-active');
+        $('.slick-track > .slick-active').removeClass('thisItem');
+        childReview.eq(2).addClass('thisItem')
+    })
+
+    /* Variables category, gallery slider */
     var categoriesLength = $('ul > #list-categories-li').length;
     var slideGallery = -1;
-    console.log(categoriesLength);
 
     /* categories filter */
     $('#list-categories-li > a').on('click', function(e) {
@@ -32,9 +69,11 @@
     });
 
     /* gallery slider arrow next*/
-    $('#next-slide-arrow').on('click', function() {
+    $('#next-slide-arrow-gallery').on('click', function() {
         if (slideGallery >= categoriesLength - 1) {
-            { slideGallery = 0 }
+            {
+                slideGallery = 0
+            }
         } else {
             slideGallery++;
         }
@@ -45,9 +84,11 @@
     })
 
     /* gallery slider arrow prev*/
-    $('#prev-slide-arrow').on('click', function() {
+    $('#prev-slide-arrow-gallery').on('click', function() {
         if (slideGallery <= 0) {
-            { slideGallery = categoriesLength - 1 }
+            {
+                slideGallery = categoriesLength - 1
+            }
         } else {
             slideGallery--;
         }
@@ -80,6 +121,8 @@
             }
         });
     });
+
+    /* Close button modal window */
     $(document).on('click', '#btn-close-modal', function() {
         var select = $('#btn-close-modal').parent();
         var select2 = select.parent();
@@ -88,4 +131,9 @@
 
 
     })
+
+    /* Review ajax slider generator */
+
+    /* Review slider arrow next */
+
 })(jQuery);
--- includes/go-script.php
+++ includes/go-script.php
@@ -2,7 +2,15 @@
 /* adding css, js  and ajax */
 function go_gallery_scripts_styles(){
 
- /* local css and js */   
+/* slick jquery carousel Plugin  */
+wp_enqueue_style('slick-carousel-css','https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css', array(),'1.9.0','all');
+wp_enqueue_script('slick-carousel-js','https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js',array('jquery'),'1.9.0',true);
+
+/* fontAwesome  */
+wp_enqueue_style('fontAwesome-css','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css', array(),'2.3.4','all');
+wp_enqueue_script('fontAwesome-js','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/js/all.min.js',array(),'2.3.4',true);
+
+/* local css and js */   
 wp_enqueue_style('go-styles-css', plugins_url('../assets/go-styles.css', __FILE__));
 wp_enqueue_script( 'go-scprit-js', plugins_url('../assets/go-script.js', __FILE__), array('jquery'), '1.0', true);
 
